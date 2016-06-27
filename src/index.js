b45ae3cdd3418f629ea8802cb89cf41e361415e9
Daniel Hunninghake <daniel@drip.fm>
2016-06-27 14:22:12 -0400

    add Button to index file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export default radon;
 export { default as Whitespace } from './css/Whitespace';
 export { default as Positioning } from './css/Positioning';
 export { default as Typography } from './css/Typography';
+export { default as Button } from './css/Button';
 export { default as Colors } from './css/Colors';
 export { default as Grid } from './css/Grid';
 
